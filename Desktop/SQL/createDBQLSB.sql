08d8e8deb808513e9ba518b30e5c94550e6d9edc
krypton99 <17520777@gm.uit.edu.vn>
2019-11-18 19:59:18 +0700

    Create Database

---
+++ Desktop/SQL/createDBQLSB.sql
@@ -0,0 +1,110 @@
+/*N'TẠO DATABASE QUẢN LÝ SÂN BANH'*/
+
+USE [master]
+GO
+
+WHILE EXISTS(select NULL from sys.databases where name='QLSB')
+BEGIN
+    DECLARE @SQL varchar(max)
+    SELECT @SQL = COALESCE(@SQL,'') + 'Kill ' + Convert(varchar, SPId) + ';'
+    FROM MASTER..SysProcesses
+    WHERE DBId = DB_ID(N'QLSB') AND SPId <> @@SPId
+    EXEC(@SQL)
+    DROP DATABASE [QLSB]
+END
+GO
+
+/* Collation = SQL_Latin1_General_CP1_CI_AS */
+CREATE DATABASE [QLSB]
+GO
+USE [QLSB]
+GO
+SET DATEFORMAT DMY
+/*N'TẠO BẢNG TÀI KHOẢN	'*/
+CREATE TABLE [dbo].[tblTAIKHOAN](
+[maTaiKhoan] [nvarchar](5) NOT NULL,
+[userName] [nvarchar](30) NOT NULL,
+[passWord] [nvarchar](30) NOT NULL,
+ CONSTRAINT [PK_tblTAIKHOAN] PRIMARY KEY CLUSTERED 
+(
+	[maTaiKhoan] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+/*N'TẠO BẢNG NHÂN VIÊN'*/
+CREATE TABLE [dbo].[tblNHANVIEN](
+[maNhanVien] [nvarchar](5) NOT NULL,
+[maTaiKhoan] [nvarchar](5) NOT NULL,
+[hoTen] [nvarchar](20) NOT NULL,
+[tuoi] [float] NOT NULL,
+[gioiTinh] [nvarchar](20) NOT NULL,
+[diaChi] [nvarchar](40) NOT NULL,
+[soDT] [nvarchar](10) NOT NULL,
+CONSTRAINT [PK_tblNHANVIEN] PRIMARY KEY CLUSTERED 
+(
+	[maNhanVien] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+CONSTRAINT fk_TAIKHOAN_NHANVIEN
+   FOREIGN KEY (maTaiKhoan)
+   REFERENCES tblTAIKHOAN (maTaiKhoan)
+) ON [PRIMARY]
+
+/*N'TẠO BẢNG KHÁCH HÀNG'*/
+CREATE TABLE [dbo].[tblKHACHHANG](
+	[maKH] [nvarchar](5) NOT NULL,
+	[tenKH] [nvarchar] (20) NOT NULL,
+	[gioiTinh] [nvarchar](5) NOT NULL,
+	[diaChi] [nvarchar](30) NOT NULL,
+	[loaiKH] [nvarchar](10) NOT NULL,
+	[soDT] [nvarchar](10) NOT NULL,
+ CONSTRAINT [PK_tblKHACHHANG] PRIMARY KEY CLUSTERED 
+(
+	[maKH] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+/*N'TẠO BẢNG PHIẾU ĐẶT SÂN'*/
+CREATE TABLE [dbo].[tblPHIEUDATSAN](
+[maPhieuDatSan] [nvarchar] (5) NOT NULL,
+[maNhanVien] [nvarchar] (5) NOT NULL,
+[maKH] [nvarchar] (5) NOT NULL,
+CONSTRAINT [PK_tblPHIEUDATSAN] PRIMARY KEY CLUSTERED 
+(
+	[maPhieuDatSan] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+CONSTRAINT fk_NHANVIEN_PHIEUDATSAN
+   FOREIGN KEY (maNhanVien)
+   REFERENCES tblNHANVIEN (maNhanVien),
+CONSTRAINT fk_KHACHHANG_PHIEUDATSAN
+   FOREIGN KEY (maKH)
+   REFERENCES tblKHACHHANG (maKH)
+) ON [PRIMARY]
+
+/*N'TẠO BẢNG SÂN'*/
+CREATE TABLE [dbo].[tblSAN](
+	[maSan] [nvarchar](5) NOT NULL,
+	[tinhTrang] [nvarchar](10),
+
+ CONSTRAINT [PK_tblSAN] PRIMARY KEY CLUSTERED 
+(
+	[maSan] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+/*N'TẠO BẢNG CHI TIẾT ĐẶT SÂN'*/
+CREATE TABLE [dbo].[tblCHITIETDATSAN](
+	[maPhieuDatSan] [nvarchar](5) NOT NULL,
+	[maSan] [nvarchar](5) NOT NULL,
+	[ngayDat] [smalldatetime] NOT NULL,
+	[gioDat] [smalldatetime] NOT NULL,
+	[thoiLuong] [float] NOT NULL,
+ CONSTRAINT [PK_tblCHITIETDATSAN] PRIMARY KEY CLUSTERED 
+(
+	[maPhieuDatSan] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+CONSTRAINT fk_PHIEUDATSAN_CHITIETDATSAN
+   FOREIGN KEY (maPhieuDatSan)
+   REFERENCES tblPHIEUDATSAN (maPhieuDatSan),
+CONSTRAINT fk_SAN_CHITIETDATSAN
+   FOREIGN KEY (maSan)
+   REFERENCES tblSAN (maSan)
+) ON [PRIMARY]
+
+
