36d44ce50e3a8b06c2a515be6b09c984f504374b
TaiTrien <v.v.t.trien@gmail.com>
2019-11-20 22:22:05 +0700

    Init project

---
+++ Mini-soccer-field-manager/GUI_MANAGER/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GUI_MANAGER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
