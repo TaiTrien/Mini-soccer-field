1b0a9002211664ba0ba0acfc20d506973c72f7c4
krypton99 <17520777@gm.uit.edu.vn>
2019-12-03 23:41:55 +0700

    Fix bugs in Database

--- Desktop/SQL/createDBQLSB.sql
+++ Desktop/SQL/createDBQLSB.sql
@@ -25,6 +25,7 @@ CREATE TABLE [dbo].[tblTAIKHOAN](
 [maTaiKhoan] [nvarchar](5) NOT NULL,
 [userName] [nvarchar](30) NOT NULL,
 [passWord] [nvarchar](30) NOT NULL,
+[Role] [nvarchar](30) NOT NULL,
  CONSTRAINT [PK_tblTAIKHOAN] PRIMARY KEY CLUSTERED 
 (
 	[maTaiKhoan] ASC
@@ -35,7 +36,7 @@ CREATE TABLE [dbo].[tblNHANVIEN](
 [maNhanVien] [nvarchar](5) NOT NULL,
 [maTaiKhoan] [nvarchar](5) NOT NULL,
 [hoTen] [nvarchar](20) NOT NULL,
-[tuoi] [float] NOT NULL,
+[tuoi] [int] NOT NULL,
 [gioiTinh] [nvarchar](20) NOT NULL,
 [diaChi] [nvarchar](40) NOT NULL,
 [soDT] [nvarchar](10) NOT NULL,
@@ -81,7 +82,7 @@ CONSTRAINT fk_KHACHHANG_PHIEUDATSAN
 /*N'TẠO BẢNG SÂN'*/
 CREATE TABLE [dbo].[tblSAN](
 	[maSan] [nvarchar](5) NOT NULL,
-	[tinhTrang] [nvarchar](10),
+	[tinhTrang] [bit],
 
  CONSTRAINT [PK_tblSAN] PRIMARY KEY CLUSTERED 
 (
@@ -92,13 +93,9 @@ CREATE TABLE [dbo].[tblSAN](
 CREATE TABLE [dbo].[tblCHITIETDATSAN](
 	[maPhieuDatSan] [nvarchar](5) NOT NULL,
 	[maSan] [nvarchar](5) NOT NULL,
-	[ngayDat] [smalldatetime] NOT NULL,
-	[gioDat] [smalldatetime] NOT NULL,
-	[thoiLuong] [float] NOT NULL,
- CONSTRAINT [PK_tblCHITIETDATSAN] PRIMARY KEY CLUSTERED 
-(
-	[maPhieuDatSan] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+	[ngayDat] [date] NOT NULL,
+	[gioDat] [time] NOT NULL,
+	[thoiLuong] [time] NOT NULL,
 CONSTRAINT fk_PHIEUDATSAN_CHITIETDATSAN
    FOREIGN KEY (maPhieuDatSan)
    REFERENCES tblPHIEUDATSAN (maPhieuDatSan),
@@ -106,5 +103,77 @@ CONSTRAINT fk_SAN_CHITIETDATSAN
    FOREIGN KEY (maSan)
    REFERENCES tblSAN (maSan)
 ) ON [PRIMARY]
-
-
+/*N'TẠO BẢNG THAM SỐ'*/
+CREATE TABLE [dbo].[tblTHAMSO](
+	[luongnhanvien] [float] NOT NULL,
+	[tiengiuxe] [float] ,
+	[giasanngay] [float] NOT NULL,
+	[giasandem] [float] NOT NULL,
+	[sotiencoc] [float] )
+/*N'TẠO BẢNG CSVC'*/
+CREATE TABLE [dbo].[tblCSVC](
+	[maCSVC] [nvarchar](5) NOT NULL,
+	[maNhanVien] [nvarchar](5) NOT NULL,
+	[tencsvc] [nvarchar](20) NOT NULL,
+	[soluongnhap] [int] NOT NULL,
+	[dongianhap] [int] NOT NULL,
+	[tinhtrangcsvc] [nvarchar](20) NOT NULL ,
+	CONSTRAINT [PK_tblCSVC] PRIMARY KEY CLUSTERED 
+(
+	[maCSVC] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+CONSTRAINT fk_NHANVIEN_CSVC
+   FOREIGN KEY (maNhanVien)
+   REFERENCES tblNHANVIEN (maNhanVien)
+) ON [PRIMARY]
+/*N'TẠO BẢNG ĐỒ UỐNG'*/
+CREATE TABLE [dbo].[tblDOUONG](
+	[maDoUong] [nvarchar](5) NOT NULL,
+	[tendouong] [nvarchar](10) NOT NULL,
+	[soluongconlai] [int] NOT NULL,
+	[dongianhap] [int] NOT NULL,
+	[dongiaban] [int] NOT NULL,
+	[ngaytaohoadon] [smalldatetime] NOT NULL,
+CONSTRAINT [PK_tblDOUONG] PRIMARY KEY CLUSTERED 
+(
+	[maDouong] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+) ON [PRIMARY]
+/*N'TẠO BẢNG HÓA ĐƠN'*/
+CREATE TABLE [dbo].[tblHOADON](
+	[maHoaDon] [nvarchar](5) NOT NULL,
+	[maNhanvien] [nvarchar](5) NOT NULL,
+	[maKH] [nvarchar](5) NOT NULL,
+	[ngaytaohoadon] [date] NOT NULL,
+CONSTRAINT [PK_tblHOADON] PRIMARY KEY CLUSTERED 
+(
+	[maHoaDon] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+CONSTRAINT fk_NHANVIEN_HOADON
+   FOREIGN KEY (maNhanVien)
+   REFERENCES tblNHANVIEN (maNhanVien),
+CONSTRAINT fk_KHACHHANG_HOADON
+   FOREIGN KEY (maKH)
+   REFERENCES tblKHACHHANG (maKH)
+) ON [PRIMARY]	
+/*N'TẠO BẢNG LOẠI HÓA ĐƠN'*/
+CREATE TABLE [dbo].[tblLOAIHOADON](
+	[maLoaiHoaDon] [nvarchar](5) NOT NULL,
+	[loaihoadon] [nvarchar](10) NOT NULL,
+CONSTRAINT [PK_tblLOAIHOADON] PRIMARY KEY CLUSTERED 
+(
+	[maLoaiHoaDon] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+) ON [PRIMARY]
+/*N'TẠO BẢNG CHI TIẾT HÓA ĐƠN'*/
+CREATE TABLE [dbo].[tblCHITIETHOADON](
+	[maLoaiHoaDon] [nvarchar](5) NOT NULL,
+	[maHoaDon] [nvarchar](5) NOT NULL,
+	[trigiahoadon] [float] NOT NULL,
+CONSTRAINT fk_HOADON_CHITIETHOADON
+   FOREIGN KEY (maHoaDon)
+   REFERENCES tblHOADON (maHoaDon),
+CONSTRAINT fk_LOAIHOADON_CHITIETHOADON
+   FOREIGN KEY (maLoaiHoaDon)
+   REFERENCES tblLOAIHOADON (maLoaiHoaDon),
+) ON [PRIMARY]
--- Desktop/SQL/testDBQLSB.sql
+++ Desktop/SQL/testDBQLSB.sql
@@ -7,8 +7,8 @@ GO
 SET DATEFORMAT DMY
 /*N'Tạo giá trị mặc định'*/
 /*N'Test bảng TAIKHOAN'*/
-INSERT INTO [tblTAIKHOAN] ([maTaiKhoan],[userName],[passWord]) VALUES (1,N'lydaonam',N'namlydao123')
-INSERT INTO [tblTAIKHOAN] ([maTaiKhoan],[userName],[passWord]) VALUES (2,N'taitrien',N'taitrien99')
+INSERT INTO [tblTAIKHOAN] ([maTaiKhoan],[userName],[passWord],[Role]) VALUES (1,N'lydaonam',N'namlydao123','master')
+INSERT INTO [tblTAIKHOAN] ([maTaiKhoan],[userName],[passWord],[Role]) VALUES (2,N'taitrien',N'taitrien99','master')
 /*N'Test bảng NHANVIEN'*/
 INSERT INTO [tblNHANVIEN] ([maNhanVien],[maTaiKhoan],[hoTen],[tuoi],[diaChi],[gioiTinh],[soDT]) VALUES (1,1,N'Lý Đạo Nam',20,N'01 Trần Duy Hưng, Hà Nội =)))','Nam','0969696969')
 INSERT INTO [tblNHANVIEN] ([maNhanVien],[maTaiKhoan],[hoTen],[tuoi],[diaChi],[gioiTinh],[soDT]) VALUES (2,2,N'Võ Văn Tài Triển',20,N'?????','Nam','0977777777')
@@ -17,7 +17,18 @@ INSERT INTO [tblKHACHHANG] ([maKH],[tenKH],[loaiKH],[diaChi],[gioiTinh],[soDT]) 
 /*N'Test bảng PHIEUDATSAN'*/
 INSERT INTO [tblPHIEUDATSAN] ([maPhieuDatSan], [maNhanVien],[maKH]) VALUES (1,2,1)
 /*N'Test bảng SAN'*/
-INSERT INTO [tblSAN] ([maSan], [tinhTrang]) VALUES (1,N'Trống')
+INSERT INTO [tblSAN] ([maSan], [tinhTrang]) VALUES (1,0)
 /*N'Test bảng CHITIETDATSAN'*/
-INSERT INTO [tblCHITIETDATSAN] ([maPhieuDatSan],[maSan],[ngayDat],[gioDat],[thoiLuong]) VALUES (1,1,'18/11/2019','20:00',2)
-
+INSERT INTO [tblCHITIETDATSAN] ([maPhieuDatSan],[maSan],[ngayDat],[gioDat],[thoiLuong]) VALUES (1,1,'2019-03-03','20:00','00:30')
+/*N'Test bảng THAMSO'*/
+INSERT INTO [tblTHAMSO] ([luongnhanvien],[tiengiuxe],[giasanngay],[giasandem],[sotiencoc]) VALUES (10000,5000,30000,50000,10000)
+/*N'Test bảng HOADON'*/
+INSERT INTO [tblHOADON] ([maHoaDon],[maNhanVien],[maKH],[ngaytaohoadon]) VALUES (1,1,1,'2019-03-03')
+/*N'Test bảng LOAIHOADON'*/
+INSERT INTO [tblLOAIHOADON] ([maLoaiHoaDon],[loaihoadon]) VALUES (1,'thu san')
+/*N'Test bảng CHITIETHOADON'*/
+INSERT INTO [tblCHITIETHOADON] ([maLoaiHoaDon],[maHoaDon],[trigiahoadon]) VALUES (1,1,100000)
+/*N'Test bảng CSVC'*/
+INSERT INTO [tblCSVC] ([maCSVC],[maNhanVien],[tencsvc],[soluongnhap],[dongianhap],[tinhtrangcsvc]) VALUES (1,1,'banh',5,50000,'hu hong')
+/*N'Test bảng DOUONG'*/
+INSERT INTO [tblDOUONG] ([maDoUong],[tendouong],[soluongconlai],[dongianhap],[dongiaban],[ngaytaohoadon]) VALUES (1,'nuoc suoi',5,5000,10000,'2019-03-03')
