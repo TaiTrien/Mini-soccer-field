3bea4ba1b64746518cb544f72a13e848ca846dc2
Ngovi <44963314+ngovi2298@users.noreply.github.com>
2019-12-15 23:35:25 +0700

    push BUS_MANAGER

---
+++ Mini-soccer-field-manager/BUS_MANAGER/parametersBUS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO_MANAGER;
+using DAL_MANAGER;
+
+namespace BUS_MANAGER
+{
+    public class parametersBUS
+    {
+        public parametersDTO Selected()
+        {
+            parametersDAL parametersDAL = new parametersDAL();
+            parametersDTO res = new parametersDTO();
+            res = parametersDAL.selectedRegulations();
+            return res;
+
+        }
+        //public bool edit(parametersDTO pr)
+        //{
+        //    parametersDAL parametersDAL = new parametersDAL();
+        //    bool res = parametersDAL.CapNhat(pr);
+        //    return res;
+
+        //}
+    }
+}
