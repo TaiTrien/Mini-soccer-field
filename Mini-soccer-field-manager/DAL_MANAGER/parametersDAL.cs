55223ac87e321cf0e92252b38ea85b8b2131d578
TaiTrien <v.v.t.trien@gmail.com>
2019-12-16 15:28:15 +0700

    Create windown command file and update paramenter table

--- Mini-soccer-field-manager/DAL_MANAGER/parametersDAL.cs
+++ Mini-soccer-field-manager/DAL_MANAGER/parametersDAL.cs
@@ -59,5 +59,130 @@ namespace DAL_MANAGER
             }
             return para;
         }
+        public List<drinksDTO> selectDrinks()
+        {
+            string query = string.Empty;
+            query += "SELECT maDoUong, dongianhap, dongiaban ";
+            query += "FROM tblDOUONG ";
+
+            List<drinksDTO> lsDrinks = new List<drinksDTO>();
+
+            using (SqlConnection con = new SqlConnection(@"server=" + Dns.GetHostName() + ";Trusted_Connection=yes;database=QLSB;"))
+
+            {
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = query;
+
+                    try
+                    {
+                        con.Open();
+                        SqlDataReader reader = null;
+                        reader = cmd.ExecuteReader();
+                        if (reader.HasRows == true)
+                        {
+                            while (reader.Read())
+                            {
+                                drinksDTO drinks = new drinksDTO();
+                                drinks.MaDoUong = reader["MaDoUong"].ToString();
+                                drinks.DonGiaMua = Convert.ToInt32(reader["dongianhap"]);
+                                drinks.DonGiaBan = Convert.ToInt32(reader["dongiaban"]);
+                                lsDrinks.Add(drinks);
+                            }
+                        }
+
+                        con.Close();
+                        con.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        con.Close();
+                        return null;
+                    }
+                }
+            }
+            return lsDrinks;
+        }
+        public bool updateDrinks(List<drinksDTO> drinks)
+        {
+            for (int i = 0; i < drinks.Count; i++)
+            {
+                string query = string.Empty;
+                query += "UPDATE tblDOUONG SET dongianhap = @dgn, dongiaban = @dgb ";
+                query += "WHERE maDoUong = @maDoUong";
+
+                using (SqlConnection con = new SqlConnection(@"server=" + Dns.GetHostName() + ";Trusted_Connection=yes;database=QLSB;")) //Init connection to host
+                {
+
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = con;
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.CommandText = query;
+
+                        cmd.Parameters.AddWithValue("@maDoUong", drinks[i].MaDoUong);
+                        cmd.Parameters.AddWithValue("@dgn", drinks[i].DonGiaMua);
+                        cmd.Parameters.AddWithValue("@dgb", drinks[i].DonGiaBan);
+
+                        try
+                        {
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+                            con.Dispose();
+                        }
+                        catch (Exception ex)
+                        {
+                            con.Close();
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+
+        }
+        public bool updatePrice(parametersDTO pr)
+        {
+          
+                string query = string.Empty;
+                query += "UPDATE tblTHAMSO ";
+                query += "SET luongnhanvien = @lnv, tiengiuxe = @tgx, giasanngay = @gsn, ";
+                query += "giasandem = @gsd, sotiencoc = @stc";
+
+                using (SqlConnection con = new SqlConnection(@"server=" + Dns.GetHostName() + ";Trusted_Connection=yes;database=QLSB;")) //Init connection to host
+                {
+
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = con;
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.CommandText = query;
+                        cmd.Parameters.AddWithValue("@lnv", pr.LuongNhanVien);
+                        cmd.Parameters.AddWithValue("@tgx", pr.GiaGiuXe);
+                        cmd.Parameters.AddWithValue("@gsn", pr.GiaSanNgay);
+                        cmd.Parameters.AddWithValue("@gsd", pr.GiaSanDem);
+                        cmd.Parameters.AddWithValue("@stc", pr.GiaCoc);
+
+                        try
+                        {
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+                            con.Dispose();
+                        }
+                        catch (Exception ex)
+                        {
+                            con.Close();
+                            return false;
+                        }
+                    }
+
+                return true;
+            }
+        }
     }
 }
